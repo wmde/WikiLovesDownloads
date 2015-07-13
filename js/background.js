f21895e1649eada6691429023962ecc747c102bd
Andrew-WMDE <andrew.kostka@wikimedia.de>
2015-07-13 13:30:57 +0200

    fixed indentations

--- js/background.js
+++ js/background.js
@@ -6,17 +6,17 @@ $( document ).ready( function() {
             license: 'amalavida.tv [CC BY-SA 2.0 (http://creativecommons.org/licenses/by-sa/2.0)], via Wikimedia Commons'
         },
 
-         {
+        {
             src: 'Penguin_in_Antarctica_jumping_out_of_the_water.jpg',
             license: 'Christopher Michel [CC BY 2.0 (http://creativecommons.org/licenses/by/2.0)], via Wikimedia Commons'
         },
 
-         {
+        {
             src: 'Swallow_flying_drinking.jpg',
             license: 'By sanchezn (Own work) [CC BY-SA 3.0 (http://creativecommons.org/licenses/by-sa/3.0)], via Wikimedia Commons'
         },
 
-         {
+        {
             src: 'Ніжний_ранковий_світло.jpg',
             license: 'By Balkhovitin (Own work) [CC BY-SA 3.0 (http://creativecommons.org/licenses/by-sa/3.0)], via Wikimedia Commons'
         },
@@ -26,7 +26,7 @@ $( document ).ready( function() {
             license: 'By Pierre Dalous (Own work) [CC BY-SA 3.0 (http://creativecommons.org/licenses/by-sa/3.0)], via Wikimedia Commons'
         },
 
-            {
+        {
             src: 'Elakala_Waterfalls_Swirling_Pool_Mossy_Rocks.jpg',
             license: 'By Forest Wander from Cross Lanes, USA (Elakala Waterfalls Swirling Pool Mossy Rocks) [CC BY-SA 2.0 (http://creativecommons.org/licenses/by-sa/2.0)], via Wikimedia Commons'
         },
