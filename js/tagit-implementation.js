afe7f90f4b05d7aa5a261eb6ae1e25ff75141bb7
Andrew-WMDE <andrew.kostka@wikimedia.de>
2015-07-15 17:34:28 +0200

    allowed for spaces to be  in a username, added a hint for using the name field, changed category example to a category currently containing more images

--- js/tagit-implementation.js
+++ js/tagit-implementation.js
@@ -1,6 +1,18 @@
 $( document ).ready( function() {
 
-    $( '#names' ).tagit();
+    $( '#names' ).tagit( {
+        allowSpaces: true
+    } );
+
     $( '.ui-helper-hidden-accessible' ).hide();
 
+    $( 'ul.tagit' )
+        .focusin( function() {
+        $( '#hint' ).show();
+        } )
+        .focusout( function() {
+            $( '#hint' ).hide();
+    } );
+
 } );
+
